99db6829abea0d6cf5926e9329b24c0cdaa39ed5
Boris Vinogradov <no111u3@gmail.com>
2019-10-11 15:36:14 +0300

    New version of Watch skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use std::io;
+use std::{io, io::Write};
 
 use tui::backend::TermionBackend;
 use tui::Terminal;
 
-use termion::raw::IntoRawMode;
+use termion::{event::Key, input::TermRead, raw::IntoRawMode, screen::AlternateScreen};
+
+use console_watch::app::Events;
+use console_watch::watch::App;
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
-    let mut _terminal = Terminal::new(backend)?;
+    let mut terminal = Terminal::new(backend)?;
+
+    let events: Events<Key> = Events::new(|_tx| || {}, |_tx| || {});
+
+    let mut app = App::new("Watch");
+    let keys = io::stdin().keys();
+
+    app.draw(&mut terminal)?;
+
+    for key in keys.filter_map(Result::ok) {
+        use Key::*;
+
+        app.draw(&mut terminal)?;
+
+        match key {
+            Char('q') | Esc => break,
+            _ => {}
+        }
+    }
+    io::stdout().flush().ok();
 
     Ok(())
 }
--- src/watch/app.rs
+++ src/watch/app.rs
@@ -1,1 +1,28 @@
+use std::io;
 
+use tui::backend::Backend;
+use tui::style::{Color, Style};
+use tui::widgets::{Paragraph, Text, Widget};
+use tui::Terminal;
+
+pub struct App<'a> {
+    pub title: &'a str,
+}
+
+impl<'a> App<'a> {
+    pub fn new(title: &'a str) -> App<'a> {
+        App { title }
+    }
+
+    pub fn draw<B: Backend>(&self, terminal: &mut Terminal<B>) -> Result<(), io::Error> {
+        terminal.draw(|mut f| {
+            let standard = Style {
+                fg: Color::Black,
+                bg: Color::Yellow,
+                ..Default::default()
+            };
+            let size = f.size();
+            Paragraph::new([Text::Styled(self.title.into(), standard)].iter()).render(&mut f, size);
+        })
+    }
+}
